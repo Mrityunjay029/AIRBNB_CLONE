0eac708365144ae5663dd57836e97029c7b4f116
Mrityunjay029 <mrityunjaypathak050@gmail.com>
2022-07-08 13:57:55 +0530

    Updates

---
+++ precache-manifest.4b8ba030d0c13ea8ef3bd6353d0c7df6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "62bbdfc14fc674adfb9b",
+    "url": "/AIRBNB_CLONE/static/css/main.b3aaf611.chunk.css"
+  },
+  {
+    "revision": "62bbdfc14fc674adfb9b",
+    "url": "/AIRBNB_CLONE/static/js/main.62bbdfc1.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/AIRBNB_CLONE/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "e0bbc1f89977b185ba34",
+    "url": "/AIRBNB_CLONE/static/js/2.e0bbc1f8.chunk.js"
+  },
+  {
+    "revision": "0641709c6f9bc1c39429",
+    "url": "/AIRBNB_CLONE/static/js/runtime~main.0641709c.js"
+  },
+  {
+    "revision": "8edd730ff134b50053eead431ebd1a4d",
+    "url": "/AIRBNB_CLONE/static/media/Airbnb-logo.8edd730f.jpg"
+  },
+  {
+    "revision": "96eda85bcfd14204560d30da1a946bfd",
+    "url": "/AIRBNB_CLONE/static/media/Group 77.96eda85b.png"
+  },
+  {
+    "revision": "3fafb97d10bcc4ad9994499024ed6bcf",
+    "url": "/AIRBNB_CLONE/static/media/Group 65 (1).3fafb97d.png"
+  },
+  {
+    "revision": "4966f35603cf7314474e599d9b5a20c0",
+    "url": "/AIRBNB_CLONE/static/media/Group 78.4966f356.png"
+  },
+  {
+    "revision": "5e3761bf26978d3114474ebfac97d4c8",
+    "url": "/AIRBNB_CLONE/static/media/Group 79.5e3761bf.png"
+  },
+  {
+    "revision": "8120924d3583fe27c5316cd82560643c",
+    "url": "/AIRBNB_CLONE/static/media/katie.8120924d.png"
+  },
+  {
+    "revision": "50ecdfcd49e7ac979621d87eb20cd1e5",
+    "url": "/AIRBNB_CLONE/static/media/wedding-photography 1.50ecdfcd.png"
+  },
+  {
+    "revision": "b5650b9812d3382f56818cc695aae118",
+    "url": "/AIRBNB_CLONE/static/media/mountain-bike 1.b5650b98.png"
+  },
+  {
+    "revision": "5df0a047805401a9be2900dce9082a2c",
+    "url": "/AIRBNB_CLONE/index.html"
+  }
+];
